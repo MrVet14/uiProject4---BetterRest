bf214df076a901cd9a60f73ba046050112498208
Vitali <vitali.vyucheiski@gmail.com>
2022-06-25 23:28:49 +0300

    Added ML model

--- uiProject4 - BetterRest/ContentView.swift
+++ uiProject4 - BetterRest/ContentView.swift
@@ -8,14 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var sleepAmount = 8.0
+    
     var body: some View {
-        Text("Hello, world!")
+        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
             .padding()
+            .border(.primary, width: 0.5)
     }
 }
-
+ 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 12 Pro Max")
     }
 }
